15e7b9de4936038bec44fbc19216d99fd5149238
Hari <hharan87@gmail.com>
2026-05-06 18:48:56 +0530

    rename api endpoint for clear_all_urls

--- src/contentfilter/main.py
+++ src/contentfilter/main.py
@@ -281,7 +281,7 @@ async def import_json(
     return redir_resp
 
 
-@webapp.post("/clear/", name="clear_urls")
+@webapp.post("/remove/all", name="remove_all_urls")
 async def clear_urls(
     request: Request,
     username: str = AuthDependency,
@@ -295,13 +295,13 @@ async def clear_urls(
         if num_urls:
             flash(
                 request,
-                f"Successfully deleted {num_urls} blocked URL(s) from the list",
+                f"Successfully removed {num_urls} blocked URL(s) from the list",
                 "success",
             )
         else:
             flash(request, "The list was already empty", "info")
     except Exception as exc:
-        flash(request, f"Unable to clear URLs: {exc}.", "danger")
+        flash(request, f"Unable to remove URLs: {exc}.", "danger")
     return redir_resp
 
 
